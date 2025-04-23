a2d55c1de6f83a72a3aab1d2d2f4154fb75e1e3d
Om Tambat <136994090+Onix284@users.noreply.github.com>
2025-04-23 09:49:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vibebook_yourdailymoodjournal/Screens/AddMoodScreen.kt
@@ -0,0 +1,12 @@
+package com.example.vibebook_yourdailymoodjournal.Screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.serialization.Serializable
+
+@Preview
+@Composable
+fun AddMoods(){
+    Text("Add Mood Screen")
+}
